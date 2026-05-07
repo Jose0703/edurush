9e5c7fe5c7dd8cd77f9c5b0f26ddb0b8de2144de
Jose <josechacha0703@gmail.com>
2026-05-07 16:34:54 -0500

    Proyecto EduRush

---
+++ src/main/java/com/edurush/pe/EdurushApplication.java
@@ -0,0 +1,13 @@
+package com.edurush.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edurush/pe/EdurushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edurush.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
